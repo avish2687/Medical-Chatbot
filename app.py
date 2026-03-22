93af437ccfd1e0439e3cdc1c3173cafb7535b768
Avishkar Kumar <165552332+avish2687@users.noreply.github.com>
2026-03-22 13:53:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,6 @@ def chat():
     return str(response["answer"])
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 # ─────────────────────────────────────────────────
